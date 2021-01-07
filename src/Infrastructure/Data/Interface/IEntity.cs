5be3aa9d88e76e0b4a4d21d23ddb2a267c071c66
onurkybsi <onurbpm@outlook.com>
2021-01-07 19:44:29 +0300

    Data infrastructure removed

--- src/Infrastructure/Data/Interface/IEntity.cs
+++
@@ -1,4 +0,0 @@
-namespace MessagingService.Infrastructure
-{
-    public interface IEntity { }
-}
--- src/Infrastructure/Data/Interface/IRepositorySettings.cs
+++
@@ -1,9 +0,0 @@
-namespace MessagingService.Infrastructure
-{
-    public interface IRepositorySettings
-    {
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
-        string CollectionName { get; set; }
-    }
-}
